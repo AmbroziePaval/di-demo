a42ffacdd8c1f62fe7e06c3d640cd9475f58425f
Ambrozie <ambroziepaval@gmail.com>
2018-10-01 21:08:48 +0300

    Hello world DI with Spring.

--- src/main/java/com/ambroziepaval/didemo/DiDemoApplication.java
+++ src/main/java/com/ambroziepaval/didemo/DiDemoApplication.java
@@ -1,12 +1,17 @@
 package com.ambroziepaval.didemo;
 
+import com.ambroziepaval.didemo.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiDemoApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(DiDemoApplication.class, args);
+
+        MyController myController = (MyController) applicationContext.getBean("myController");
+        myController.hello();
     }
 }
--- src/main/java/com/ambroziepaval/didemo/controllers/MyController.java
+++ src/main/java/com/ambroziepaval/didemo/controllers/MyController.java
@@ -1,7 +1,14 @@
 package com.ambroziepaval.didemo.controllers;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * Created by Ambrozie on 01/10/2018
  */
+@Controller
 public class MyController {
+
+    public void hello(){
+        System.out.println("Hello world!");
+    }
 }
